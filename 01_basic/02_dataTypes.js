5fb406cfad1e6580eac23673924cb17e5e30813b
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2023-01-09 11:37:06 +0000

    conversion and operation

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer verision
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be hight
+
+    console.log("Mohammad")
+
+    let name = "mohammad"
+    let age = 19 
+    let isLoggedIn = false
+
+    // number => 2 to power  53
+    // bigint
+    //string => ""
+    //boolean => true/false
+    //null =>standalone value
+    //undifined =>
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
